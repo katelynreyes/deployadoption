0e2fa402c055cdeb95c8d2d110762268d72f8e8f
katelynreyes <katelynreyes@unomaha.edu>
2024-05-16 18:59:47 -0500

    debug false

--- adoption_site/settings.py
+++ adoption_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i41yx_1rmns@g#)0y--ejhdp3gjb9s+r-&#9#(8y=0*7qa%sbz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'katelynreyes.pythonanywhere.com']
 
